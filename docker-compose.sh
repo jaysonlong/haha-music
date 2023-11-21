9ce79be3826fc48410ff27ebe1c08d2f5b6ca157
Jayson Long <im.lgchen@foxmail.com>
2023-11-22 04:38:49 +0800

    optimize start script

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
+function create_containers() {
+    docker network create haha-music > /dev/null 2>&1
+
+    PREV_PORT=`docker inspect --format='{{(index (index .HostConfig.PortBindings "80/tcp") 0).HostPort}}' haha-nginx 2> /dev/null`
+
+    if [ "$PORT" == "-1" ]; then
+        if [ "$PREV_PORT" != "" ]; then
+            echo remove prev nginx container
+            docker stop haha-nginx > /dev/null
+            docker rm haha-nginx > /dev/null
+        fi
+
+        docker create --name haha-nginx --net haha-music -v $(pwd)/src:/var/www/html \
+            -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx:1.24.0  > /dev/null 2>&1
+    
+    else
+        if [ "$PREV_PORT" != "" ] && [ "$PREV_PORT" != "$PORT" ]; then
+            echo remove prev nginx container
+            docker stop haha-nginx > /dev/null
+            docker rm haha-nginx > /dev/null
+        fi
+
+        docker create --name haha-nginx --net haha-music -p ${PORT}:80 -v $(pwd)/src:/var/www/html \
+            -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx:1.24.0  > /dev/null 2>&1
+    fi
+
+    docker create --name haha-php-fpm --net haha-music -v $(pwd)/src:/var/www/html bitnami/php-fpm:8.0  > /dev/null 2>&1
+}
+
+
+PORT=${PORT:-80}
+
 if [ "$1" = "up" ]; then
-    docker rm haha_php_fpm 2> /dev/null
-    docker run --name haha_php_fpm -v $(pwd)/src:/var/www/html -d bitnami/php-fpm:8.0
-
-    docker rm haha_nginx 2> /dev/null
-    docker run --name haha_nginx --link haha_php_fpm:php-fpm -p ${PORT:-80}:80 \
-        -v $(pwd)/src:/var/www/html -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf -d nginx:1.24.0
-
+    create_containers
+    docker start haha-php-fpm
+    docker start haha-nginx
 elif [ "$1" = "stop" ]; then
-    docker stop haha_php_fpm
-    docker stop haha_nginx
+    docker stop haha-php-fpm
+    docker stop haha-nginx
 fi
